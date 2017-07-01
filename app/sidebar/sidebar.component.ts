2b745ec5be4021d45f1361d632be53b7c412e393
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-01 14:28:58 +0100

    Fix bug where temp/description weren't being shown

--- app/sidebar/sidebar.component.ts
+++ app/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './profile.service';
+import { WeatherService } from './../weather/weather.service';
+import { IProfile } from './profile';
+import { IWeather } from './../weather/weather';
 
 @Component({
     moduleId: module.id,
@@ -7,7 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SidebarComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    profiles: IProfile[];
+    weatherItem: IWeather[];
+    newProfile: IProfile = {
+        profileName: '',
+        city: ''
+    };
+
+    constructor(
+        private profileService: ProfileService,
+        private weatherService: WeatherService
+    ) { }
+
+    ngOnInit() {
+        this.profiles = this.getProfiles();
+    }
+
+    getProfiles(): IProfile[] {
+        return this.profileService.getProfiles();
+    }
+
+    onSaveNew() {
+        const profileItem: IProfile = {
+            profileName: this.newProfile.profileName,
+            city: this.newProfile.city
+        };
+        this.saveProfile(profileItem)
+    }
+
+    saveProfile(profileItem: IProfile) {
+        this.profileService.saveNewProfile(profileItem);
+        this.newProfile.profileName = '';
+        this.newProfile.city = '';
+    }
+
+    onLoadProfile(profile: IProfile) {
+        this.weatherService.searchWeatherData(profile.city)
+            .subscribe(
+                data => this.weatherItem = data
+            );
+    }
+
+    onDeleteProfile($event: Event, profile: IProfile) {
+        event.stopPropagation();
+        this.profileService.deleteProfile(profile);
+    }
+
 }
